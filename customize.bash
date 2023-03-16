c7c1a47f3b89ab88dfd3a546db1872254d2044de
Sanduuz <sanduuz@iki.fi>
2023-03-16 08:00:02 +0200

    Added pass program and changed git config user.email

--- customize.bash
+++ customize.bash
@@ -72,7 +72,7 @@ debsecan lsof apt-show-versions sshfs binwalk rlwrap pavucontrol \
 manpages-dev apt-mirror dislocker d-feet strace ltrace \
 binutils-multiarch libguestfs-tools chromium memtest86+ \
 tcpdump whois wireshark openvpn socat golang nano wget \
-tshark traceroute apt-transport-https python3-requests tree
+tshark traceroute apt-transport-https python3-requests tree pass
 
 echo "Installing Sublime Text"
 wget -qO - https://download.sublimetext.com/sublimehq-pub.gpg | apt-key add -
@@ -126,7 +126,7 @@ EOF
 
 echo "Updating git information"
 run_as_user git config --global user.name "Sanduuz"
-run_as_user git config --global user.email "19jdmz5js@protonmail.ch"
+run_as_user git config --global user.email "sanduuz@iki.fi"
 
 echo "Modifying .bashrc"
 run_as_user tee -a ~$NORMAL_USER/.bashrc <<EOF
